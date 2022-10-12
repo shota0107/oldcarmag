0d90131422eea8ae96516c340e9d0e9e5aa8584d
EC2 Default User <shota8127@gmail.com>
2022-10-12 07:56:07 +0000

    user

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
     post 'users/guest_sign_in', to: 'user/sessions#guest_sign_in'
   end
   devise_for :users
+  root to: 'homes#top'
   devise_for :publics
   devise_for :admins
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
